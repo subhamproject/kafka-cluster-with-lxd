a3126bedd37115643be49e749a0c0e9b54db2f0c
Subham Mandal <subhammandal01@gmail.com>
2022-10-16 00:52:40 +0530

    Update kafka.sh

--- kafka.sh
+++ kafka.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
 
-
-check_zk () {
-HOST=$1
-PORT=$2
-echo "Waiting for $HOST to launch on port $PORT..."
-while ! timeout 1 bash -c "echo > /dev/tcp/$HOST/$PORT"; do
-  sleep 1
-done
-echo "$HOST is up on port $PORT"
-}
-
-SERVER=""
-[[ -z $SERVER ]] && { echo "Please provide Zookeeper server details" ; }
-
-if [[ -n $SERVER ]];then
-IFS=','; for name in $SERVER; do
-    IFS=':' read -a NAME <<< "$name"
-    check_zk ${NAME[0]} ${NAME[1]}
-   done
-fi
-
-
 export KAFKA_VERSION=3.2.0
 
 apt install default-jdk -y
